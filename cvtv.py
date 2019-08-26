64c1658a6431ccc406a1c5c1bab965f7d1f3ba4c
Keith Batten <kbatten@gmail.com>
2019-08-25 20:00:39 -0700

    add circle detection

--- cvtv.py
+++ cvtv.py
@@ -10,15 +10,57 @@ import numpy as np
 import cv2 as cv
 
 
+def augment_detect_circle_channel(image, image_gray, color):
+    BLUR = 5
+    image_blur = cv.medianBlur(image_gray, BLUR)
+
+    DOWNSCALE = 1
+    MIN_DIST = 20
+    HIGH_THRESHOLD = 100
+    ACCUMULATOR_THRESHOLD = 200
+    circles = cv.HoughCircles(image_blur, cv.HOUGH_GRADIENT, DOWNSCALE, MIN_DIST, param1=HIGH_THRESHOLD, param2=ACCUMULATOR_THRESHOLD, minRadius=0, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+
+    COLOR_BORDER = color
+    COLOR_CENTER = color
+    for i in circles[0,:]:
+        # border
+        cv.circle(image, (i[0], i[1]), i[2], COLOR_BORDER, 2)
+        # center
+        cv.circle(image, (i[0], i[1]), 2, COLOR_CENTER, 3)
+
+
+def augment_detect_circle(image):
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    BLUR = 5
+    image_blur = cv.medianBlur(image_gray, BLUR)
+
+    DOWNSCALE = 1
+    MIN_DIST = 20
+    HIGH_THRESHOLD = 100
+    ACCUMULATOR_THRESHOLD = 200
+    circles = cv.HoughCircles(image_blur, cv.HOUGH_GRADIENT, DOWNSCALE, MIN_DIST, param1=HIGH_THRESHOLD, param2=ACCUMULATOR_THRESHOLD, minRadius=0, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+
+    COLOR_BORDER = (0, 255, 0)
+    COLOR_CENTER = (0, 0, 255)
+    for i in circles[0,:]:
+        # border
+        cv.circle(image, (i[0], i[1]), i[2], COLOR_BORDER, 2)
+        # center
+        cv.circle(image, (i[0], i[1]), 2, COLOR_CENTER, 3)
+
+
 def augment_detect_corner(image):
     # convert to format for cornerHarris
-    image_gray = np.float32(cv.cvtColor(image, cv.COLOR_BGR2GRAY))
+    image_data_gray = np.float32(cv.cvtColor(image, cv.COLOR_BGR2GRAY))
 
     # mark corners
     BLOCKSIZE = 2
     KSIZE = 3
     K = 0.04
-    detected = cv.cornerHarris(image_gray, BLOCKSIZE, KSIZE, K)
+    detected = cv.cornerHarris(image_data_gray, BLOCKSIZE, KSIZE, K)
 
     # increase size of markers
     detected = cv.dilate(detected, None)
@@ -33,7 +75,19 @@ def main():
     filename = "/home/keith/tmp/cvtv.png"
     image = cv.imread(filename)
 
-    augment_detect_corner(image)
+    if False:
+        augment_detect_corner(image)
+
+    if False:
+        augment_detect_circle(image)
+
+    if True:
+        image_blue = image.copy()[:,:,0]
+        image_green = image.copy()[:,:,1]
+        image_red = image.copy()[:,:,2]
+        augment_detect_circle_channel(image, image_blue, [100, 100, 255]) # blue
+        augment_detect_circle_channel(image, image_green, [100, 255, 100]) # green
+        augment_detect_circle_channel(image, image_red, [255, 100, 100]) # red
 
     cv.imshow("output", image)
 
